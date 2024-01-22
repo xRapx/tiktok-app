328d3b8c133644462597d373a8e5e7d75711b324
xRapx <102133684+xRapx@users.noreply.github.com>
2024-01-22 16:48:51 +0700

    commit contructor Layout

---
+++ src/routes/index.js
@@ -0,0 +1,35 @@
+import { HeaderOnly } from '~/components/Layout';
+
+import Home from '~/pages/Home';
+import Follow from '~/pages/Follow';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    {
+        path: '/',
+        component: Home,
+    },
+    {
+        path: '/follow',
+        component: Follow,
+    },
+    {
+        path: '/profile',
+        component: Profile,
+    },
+    {
+        path: '/upload',
+        component: Upload,
+        layout: HeaderOnly,
+    },
+    {
+        path: '/search',
+        component: Search,
+        layout: null,
+    },
+];
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };

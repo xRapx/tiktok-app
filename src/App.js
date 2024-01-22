a98bed92599d4d3562a6b9b196efe4da795a05c5
xRapx <102133684+xRapx@users.noreply.github.com>
2024-01-22 12:02:58 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from "react";
-
+import React from 'react';
 
 function App() {
-
-  return (
-    <div className="App">
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>HOME PAGES</h1>
+        </div>
+    );
 }
 
 export default App;

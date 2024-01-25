9a94090574f8fe2cc4a9c4dd415e530a07deee40
xRapx <102133684+xRapx@users.noreply.github.com>
2024-01-25 15:50:58 +0700

    input style

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,8 @@
 import classNames from 'classnames/bind';
 import style from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(style);
 
@@ -7,7 +10,22 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
-                <h1>Header</h1>
+                {/*   Logo  */}
+                <img src={images.logo} alt="TikTok" />
+
+                {/*   Input Search  */}
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon className={cx('icon-search')} icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                {/*   Action Profile  */}
+                <div className={cx('action')}></div>
             </div>
         </header>
     );

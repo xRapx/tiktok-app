3cca041e95dcf2252ea829cde83b70c044707e40
xRapx <102133684+xRapx@users.noreply.github.com>
2024-02-15 12:33:21 +0700

    revert code Hoàn  thiện header #7 from F8

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,21 +1,22 @@
-import { forwardRef, useState } from 'react';
-import images from '~/assets/images';
 import classNames from 'classnames';
+import { useState, forwardRef } from 'react';
+import images from '~/assets/images';
 import style from './Image.module.scss';
 
-const Image = forwardRef(({ alt, src, className, fallback : customFallback = images.noImage, ...props }, ref) => {
+const Image = forwardRef(({ src, alt, className, fallback: customFallback = images.noImage, ...props }, ref) => {
     const [fallback, setFallback] = useState('');
+
     const handleError = () => {
         setFallback(customFallback);
     };
 
     return (
         <img
-            className={classNames(style.wrapper, className)}
             ref={ref}
+            className={classNames(style.wrapper, className)}
             src={fallback || src}
-            {...props}
             alt={alt}
+            {...props}
             onError={handleError}
         />
     );
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -9,6 +9,7 @@ import {
     faGear,
     faKeyboard,
     faSignOut,
+    // faSpinner,
     faUser,
 } from '@fortawesome/free-solid-svg-icons';
 
@@ -19,11 +20,11 @@ import Button from '~/components/Button';
 import style from './Header.module.scss';
 import images from '~/assets/images';
 import Menu from '~/components/Popper/Menu';
+import Search from '~/components/Search';
 import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icon';
 import Image from '~/components/Image';
-import img1 from '~/assets/images/img-2.jpg';
-import img2 from '~/assets/images/img-3.jpg';
-import Search from '~/components/Search';
+// import img1 from '~/assets/images/img-2.jpg';
+// import img2 from '~/assets/images/img-3.jpg';
 
 const cx = classNames.bind(style);
 
@@ -107,18 +108,10 @@ function Header() {
                 <div className={cx('action')}>
                     {currentUser ? (
                         <Fragment>
-                            <Tippy delay={[0, 200]} content="Upload" placement="bottom">
+                            <Tippy delay={[0, 200]} content="Upload video" placement="bottom">
                                 <button className={cx('action-btn')}>
                                     <UploadIcon />
-                                </button>
-                            </Tippy>
-                            <Tippy delay={[0, 200]} content="Message" placement="bottom">
-                                <button className={cx('action-btn')}>
                                     <MessageIcon />
-                                </button>
-                            </Tippy>
-                            <Tippy delay={[0, 200]} content="Inbox" placement="bottom">
-                                <button className={cx('action-btn')}>
                                     <InboxIcon />
                                 </button>
                             </Tippy>
@@ -132,7 +125,7 @@ function Header() {
 
                     <Menu items={currentUser ? userMenu : MENU_ITEM} onChange={handleMenu}>
                         {currentUser ? (
-                            <Image className={cx('user-avatar')} src={img1} alt="nguyen van a" fallback={img2} />
+                            <Image className={cx('user-avatar')} src="" alt="nguyen van a" />
                         ) : (
                             <button className={cx('btn-menu')}>
                                 <FontAwesomeIcon icon={faEllipsisVertical} />
